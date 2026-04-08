28f9d89ed3e3ef2656af9953289e3893ff818c12
Marcelin <lenforiee@gmail.com>
2026-04-08 15:31:52 +0200

    dodanie komentarzy do klasy i metod w DefaultRhymersFactory

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -5,23 +5,44 @@ import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 import edu.kis.vh.nursery.factory.RhymersFactory;
 
+/**
+ * Klasa reprezentująca fabrykę stosów
+ * Posiada metody do tworzenia wybranych fabryk
+ * Dziedziczy od interfejsu RhymersFactory.
+ */
 public class DefaultRhymersFactory implements RhymersFactory {
 
+    /**
+     * Metoda tworząca standardowy rymownik.
+     * @return DefaultCountingOutRhymer
+     */
     @Override
     public DefaultCountingOutRhymer getStandardRhymer() {
         return new DefaultCountingOutRhymer();
     }
-
+    
+    /**
+     * Metoda tworząca fałszywą rymownik.
+     * @return DefaultCountingOutRhymer
+     */
     @Override
     public DefaultCountingOutRhymer getFalseRhymer() {
         return new DefaultCountingOutRhymer();
     }
-
+    
+    /**
+     * Metoda tworząca rymownik FIFO.
+     * @return FIFORhymer
+     */
     @Override
     public DefaultCountingOutRhymer getFIFORhymer() {
         return new FIFORhymer();
     }
-
+    
+    /**
+     * Metoda tworząca rymownik Hanoi.
+     * @return HanoiRhymer
+     */
     @Override
     public DefaultCountingOutRhymer getHanoiRhymer() {
         return new HanoiRhymer();
