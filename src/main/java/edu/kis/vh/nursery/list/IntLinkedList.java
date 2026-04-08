81f58dc5b99f079172c32cadb68b58813545fc3b
Marcelin <lenforiee@gmail.com>
2026-04-08 14:59:21 +0200

    refactor: ukrycie klasy Node do IntLinkedList oraz usuniecie pliku z klasa

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -38,3 +38,34 @@ public class IntLinkedList {
     }
 
 }
+
+class Node {
+
+    private final int value;
+    private Node prev;
+    private Node next;
+
+    public Node(int i) {
+        value = i;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public Node getPrev() {
+        return this.prev;
+    }
+
+    public void setPrev(Node node) {
+        this.prev = node;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+
+    public void setNext(Node node) {
+        this.next = node;
+    }
+}
